590484c3262c60ad08da45b6f05cea862bcd7f96
Lilit Yepremyan <lilith.yepremyan@gmail.com>
2024-08-29 16:27:25 +0400

    theme switcher with render props

--- ThemeSwicher/src/App.tsx
+++ ThemeSwicher/src/App.tsx
@@ -1,19 +1,75 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [isDark, setIsDark] = useState(false);
+//   return (
+//     <div className={isDark ? "dark-mode" : "light-mode"}>
+//       <h1>Theme Switcher</h1>
+//       <button
+//         onClick={() => {
+//           setIsDark(!isDark);
+//         }}
+//       >
+//         {isDark ? "Light mode" : "Dark mode"}
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// function App() {
+//   const [isDark, setIsDark] = useState(false);
+
+//   const themeSwitcher = (isDark: boolean) => {
+//     setIsDark(!isDark);
+//   };
+
+//   return (
+//     <div className={isDark ? "dark-mode" : "light-mode"}>
+//       <h1>Theme Switcher</h1>
+//       <button
+//         onClick={() => {
+//           themeSwitcher(isDark);
+//         }}
+//       >
+//         {isDark ? "Light mode" : "Dark mode"}
+//       </button>
+//     </div>
+//   );
+// }
+
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+const ThemeSwitcher = ({ render }) => {
   const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    setIsDark(!isDark);
+  }, [isDark]);
+  return render({ isDark });
+};
+
+function App() {
   return (
-    <div className={isDark ? "dark-mode" : "light-mode"}>
-      <h1>Theme Switcher</h1>
-      <button
-        onClick={() => {
-          setIsDark(!isDark);
-        }}
-      >
-        {isDark ? "Light mode" : "Dark mode"}
-      </button>
-    </div>
+    <ThemeSwitcher
+      render={({ isDark }) => {
+        <>
+          <h1>Theme Switcher</h1>;
+          {isDark ? (
+            <div className={"dark-mode"}>
+              <button>Light mode</button>
+            </div>
+          ) : (
+            <div className={"light-mode"}>
+              <button>Dark mode</button>
+            </div>
+          )}
+        </>;
+      }}
+    />
   );
 }
 
